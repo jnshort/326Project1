777cf57f36322d8202fff8907c20eea063db2bf1
jnshort <justinnshort98@gmail.com>
2024-06-10 21:00:05 -0700

    fixed stuff

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Run filecopy on each test instance
+echo Executing "./filecopy test_inputs/test_short.txt test_outputs/test_short.txt"
 ./filecopy test_inputs/test_short.txt test_outputs/test_short.txt
+echo Executing "./filecopy test_inputs/test_medium.txt test_outputs/test_medium.txt"
 ./filecopy test_inputs/test_medium.txt test_outputs/test_medium.txt
+echo Executing "./filecopy test_inputs/test_long.txt test_outputs/test_long.txt"
 ./filecopy test_inputs/test_long.txt test_outputs/test_long.txt
 
 # Compare copies to originals (will not print anything if files match)
+echo Comparing files - will not print anything if files match
 cmp test_inputs/test_short.txt test_outputs/test_short.txt
 cmp test_inputs/test_medium.txt test_outputs/test_medium.txt
 cmp test_inputs/test_long.txt test_outputs/test_long.txt
 
-
+echo Testing Complete
